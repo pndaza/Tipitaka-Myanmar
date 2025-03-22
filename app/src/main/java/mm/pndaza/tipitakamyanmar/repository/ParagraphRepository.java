66572edd3892bb7a54f39ab676132cccf62f6af4
pndaza <mrpndaza@outlook.com>
2025-03-22 09:38:42 +0630

    add firebase crash analysis

--- app/src/main/java/mm/pndaza/tipitakamyanmar/repository/ParagraphRepository.java
+++ app/src/main/java/mm/pndaza/tipitakamyanmar/repository/ParagraphRepository.java
@@ -2,8 +2,11 @@ package mm.pndaza.tipitakamyanmar.repository;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
+import com.google.firebase.crashlytics.FirebaseCrashlytics;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,107 +27,117 @@ public class ParagraphRepository extends BaseRepository {
     private static final String PARAGRAPH_INDEX_COLUMN = "paragraph_index";
 
     private static final String TAG = "ParagraphRepository";
-/*
-    public ArrayList<Integer> getParagraphs(String bookId, int pageNumber) {
-        SQLiteDatabase database = getReadableDatabase();
-        SQLBuilder sqlBuilder = new SQLBuilder().
-
-                select("paragraph_number").from(PARAGRAPH_TABLE)
-                .where(BOOK_ID_COLUMN, "=", bookId)
-                .where(PAGE_NUMBER_COLUMN, "=", pageNumber);
-        String sql = sqlBuilder.build();
-//        Object[] args = new Object[]{bookId, pageNumber};
-        Log.d(TAG, "book id:" + bookId + " page number:" + pageNumber);
-        Log.d(TAG, "getParagraphs: " + sql);
-        Cursor cursor = database.rawQuery(sql, null);
-        ArrayList<Integer> paraNumbers = new ArrayList<>();
-        if (cursor != null && cursor.moveToFirst()) {
-            do {
-                paraNumbers.add(cursor.getInt(0));
-            } while (cursor.moveToNext());
-            cursor.close();
-            Log.d(TAG, "getParagraphs: " + paraNumbers);
-        }
-        return paraNumbers;
-    }
-    */
 
     public ArrayList<Paragraph> getParagraphs(String bookId, int pageNumber) {
-        SQLiteDatabase sqLiteDatabase = getReadableDatabase();
-        ArrayList<Paragraph> paragraphs = new ArrayList<Paragraph>();
-//        String sql = new SQLBuilder().
-//                select(PARAGRAPH_NUMBER_COLUMN,  PARAGRAPH_INDEX_COLUMN).
-//                from(PARAGRAPH_TABLE).
-//                where(BOOK_ID_COLUMN, "=", bookId).
-//                build();
+        ArrayList<Paragraph> paragraphs = new ArrayList<>();
         String sql = "SELECT paragraph_number, paragraph_index FROM paragraphs " +
                 " WHERE book_id = ? AND page_number = ?";
         Log.d(TAG, "getParagraphs: " + sql);
-        Cursor cursor = sqLiteDatabase.rawQuery(sql, new String[]{bookId, String.valueOf(pageNumber)});
-        if (cursor != null && cursor.moveToFirst()) {
-            do {
-
-                final int paragraphNumber = cursor.getInt(cursor.getColumnIndexOrThrow(PARAGRAPH_NUMBER_COLUMN));
-                final int paragraphIndex = cursor.getInt(cursor.getColumnIndexOrThrow(PARAGRAPH_INDEX_COLUMN));
-                Paragraph paragraph = new Paragraph(paragraphNumber, paragraphIndex);
-                paragraphs.add(paragraph);
-            } while (cursor.moveToNext());
-            cursor.close();
+        SQLiteDatabase sqLiteDatabase = getReadableDatabase();
+        try (Cursor cursor = sqLiteDatabase.query(PARAGRAPH_TABLE,
+                new String[]{PARAGRAPH_NUMBER_COLUMN, PARAGRAPH_INDEX_COLUMN},
+                BOOK_ID_COLUMN + " = ? AND " + PAGE_NUMBER_COLUMN + " = ?",
+                new String[]{bookId, String.valueOf(pageNumber)},
+                null, null, null)) {
+            if (cursor != null && cursor.moveToFirst()) {
+                do {
+                    final int paragraphNumber = cursor.getInt(cursor.getColumnIndexOrThrow(PARAGRAPH_NUMBER_COLUMN));
+                    final int paragraphIndex = cursor.getInt(cursor.getColumnIndexOrThrow(PARAGRAPH_INDEX_COLUMN));
+                    Paragraph paragraph = new Paragraph(paragraphNumber, paragraphIndex);
+                    paragraphs.add(paragraph);
+                } while (cursor.moveToNext());
+            }
+        } catch (SQLiteException e) {
+            // Log the error to Crashlytics
+            FirebaseCrashlytics crashlytics = FirebaseCrashlytics.getInstance();
+            crashlytics.log("Error fetching paragraphs for bookId: " + bookId + " and page number: " + pageNumber);
+            crashlytics.recordException(e);
         }
+
         return paragraphs;
     }
 
 
     public int getFirstParagraph(String bookId) {
         SQLiteDatabase database = getReadableDatabase();
-//        String query = "SELECT paragraph_number FROM paragraphs WHERE book_id = ? ORDER BY paragraph_number ASC LIMIT 1";
         String query = "SELECT min(paragraph_number) FROM paragraphs WHERE book_id = ?";
-        Cursor cursor = database.rawQuery(query, new String[]{bookId});
 
-        if (cursor != null && cursor.moveToFirst()) {
-            int paraNumber = cursor.getInt(0);
-            cursor.close();
-            Log.d(TAG, "getFirstParagraphs: " + paraNumber);
-            return paraNumber;
+        try (Cursor cursor = database.rawQuery(query, new String[]{bookId})) {
+            if (cursor != null && cursor.moveToFirst()) {
+                int paraNumber = cursor.getInt(0);
+                Log.d(TAG, "getFirstParagraphs: " + paraNumber);
+                return paraNumber;
+            }
+        } catch (SQLiteException e) {
+            // Log the error to Crashlytics
+            FirebaseCrashlytics crashlytics = FirebaseCrashlytics.getInstance();
+            crashlytics.log("Error fetching first paragraph for bookId: " + bookId);
+            crashlytics.recordException(e);
         }
+
         return 0;
     }
 
     public int getLastParagraph(String bookId) {
         SQLiteDatabase database = getReadableDatabase();
-//        String query = "SELECT paragraph_number FROM paragraphs WHERE book_id = ? ORDER BY rowid DESC LIMIT 1";
         String query = "SELECT max(paragraph_number) FROM paragraphs WHERE book_id = ?";
-        Cursor cursor = database.rawQuery(query, new String[]{bookId});
-        if (cursor != null && cursor.moveToFirst()) {
-            int paraNumber = cursor.getInt(0);
-            cursor.close();
-            return paraNumber;
+        try (Cursor cursor = database.rawQuery(query, new String[]{bookId})) {
+            if (cursor != null && cursor.moveToFirst()) {
+                int paraNumber = cursor.getInt(0);
+                Log.d(TAG, "getLastParagraph: " + paraNumber);
+                return paraNumber;
+            }
+        } catch (SQLiteException e) {
+            // Log the error to Crashlytics
+            FirebaseCrashlytics crashlytics = FirebaseCrashlytics.getInstance();
+            crashlytics.log("Error fetching last paragraph for bookId: " + bookId);
+            crashlytics.recordException(e);
         }
         return 0;
     }
 
     public int getPageNumber(String bookId, int paragraphNumber) {
         SQLiteDatabase database = getReadableDatabase();
-        String query = "SELECT page_number FROM paragraphs " +
-                "WHERE book_id = ? AND paragraph_number = ?";
-        Cursor cursor = database.rawQuery(query, new String[]{bookId, String.valueOf(paragraphNumber)});
-        if (cursor != null && cursor.moveToFirst()) {
-            int pageNumber = cursor.getInt(0);
-            cursor.close();
-            return pageNumber;
+        try (Cursor cursor = database.query(PARAGRAPH_TABLE,
+                new String[]{PAGE_NUMBER_COLUMN},
+                BOOK_ID_COLUMN + " = ? AND " + PARAGRAPH_NUMBER_COLUMN + " = ?",
+                new String[]{bookId, String.valueOf(paragraphNumber)},
+                null, null, null)) {
+            if (cursor != null && cursor.moveToFirst()) {
+                int pageNumber = cursor.getInt(0);
+                Log.d(TAG, "getPageNumber: " + pageNumber);
+                return pageNumber;
+            }
+        } catch (SQLiteException e) {
+            // Log the parameters to Crashlytics
+            FirebaseCrashlytics crashlytics = FirebaseCrashlytics.getInstance();
+            crashlytics.log("bookId: " + bookId);
+            crashlytics.log("paragraphNumber: " + paragraphNumber);
+            crashlytics.recordException(e); // Send the exception to Crashlytics
         }
         return 0;
     }
 
-    public int getPageNumber(String bookId, int paragraphNumber, int paragraphIndex) {
+    public int getPageNumber(String bookId, int paragraphNumber,
+                             int paragraphIndex) {
         SQLiteDatabase database = getReadableDatabase();
-        String query = "SELECT page_number FROM paragraphs " +
-                "WHERE book_id = ? AND paragraph_number = ? AND paragraph_index = ?";
-        Cursor cursor = database.rawQuery(query, new String[]{bookId, String.valueOf(paragraphNumber), String.valueOf(paragraphIndex)});
-        if (cursor != null && cursor.moveToFirst()) {
-            int pageNumber = cursor.getInt(0);
-            cursor.close();
-            return pageNumber;
+        try (Cursor cursor = database.query(PARAGRAPH_TABLE,
+                new String[]{PAGE_NUMBER_COLUMN},
+                BOOK_ID_COLUMN + " = ? AND " + PARAGRAPH_NUMBER_COLUMN + " = ? AND " + PARAGRAPH_INDEX_COLUMN + " = ?",
+                new String[]{bookId, String.valueOf(paragraphNumber), String.valueOf(paragraphIndex)},
+                null, null, null)) {
+            if (cursor != null && cursor.moveToFirst()) {
+                int pageNumber = cursor.getInt(0);
+                cursor.close();
+                return pageNumber;
+            }
+        } catch (SQLiteException e) {
+            // Log the parameters to Crashlytics
+            FirebaseCrashlytics crashlytics = FirebaseCrashlytics.getInstance();
+            crashlytics.log("bookId: " + bookId);
+            crashlytics.log("paragraphNumber: " + paragraphNumber);
+            crashlytics.log("paragraphIndex: " + paragraphIndex);
+            crashlytics.recordException(e); // Send the exception to Crashlytics
         }
         return 0;
     }
